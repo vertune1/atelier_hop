85206ff5d29b95b8993b751d34d47ec9b5de4fba
vertune1 <paul.koennicke@hotmail.com>
2025-01-21 11:33:07 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Einfaches Beispiel, um den Absende-Button im Kontaktformular zu aktivieren
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formular abgesendet!');
-});
